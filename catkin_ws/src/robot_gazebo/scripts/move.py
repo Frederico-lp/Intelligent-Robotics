d72b6675f7b70f7f8625b386ff8365c953c1489d
luismrguimaraes <guimaraesluis0@gmail.com>
2023-10-17 15:16:45 +0000

    feat: improve wall following algorithm

--- catkin_ws/src/robot_gazebo/scripts/move.py
+++ catkin_ws/src/robot_gazebo/scripts/move.py
@@ -26,10 +26,10 @@ class TurtleBot:
 		self.originaltime = rospy.Time.now().to_sec()
 		self.state = 0
 
-		self.min_dist = 0.1
+		self.min_dist = 0.15
 		self.max_dist = 0.7
 
-		self.ideal_dist = 0.3
+		self.target_dist = 0.3
 
 		rospy.on_shutdown(self.shutdown)
 
@@ -40,18 +40,11 @@ class TurtleBot:
 		velocity.angular.z = 0.3
 		return velocity
 	
-
 	def turn_right(self):
 		velocity = Twist()
 		velocity.linear.x = 0
 		velocity.angular.z = -0.3
 		return velocity
-
-
-		'''
-		Function: move_ahead:  This function publishes linear and angular velocities for moving straight.
-		'''
-
 
 	def move_ahead(self):
 		velocity = Twist()
@@ -59,36 +52,25 @@ class TurtleBot:
 		velocity.angular.z = 0
 		return velocity
 
-
-		'''
-		Function: move_diag_right:  This function publishes linear and angular velocities for moving diagonally right.
-		'''
-
-
 	def move_diag_right(self):
 		velocity = Twist()
 		velocity.linear.x = 0.1
-		velocity.angular.z = -0.5
+		velocity.angular.z = -0.7
 		return velocity
-
-
-		'''
-		Function: move_diag_left:  This function publishes linear and angular velocities for moving diagonally left.
-		'''
-
 
 	def move_diag_left(self):
 		velocity = Twist()
 		velocity.linear.x = 0.1
-		velocity.angular.z = 0.5
+		velocity.angular.z = 0.7
 		return velocity
-	
 
 	def move_backwards(self):
 		velocity = Twist()
 		velocity.linear.x = -0.1
 		return velocity
 	
+	def stop(self):
+		return Twist()
 		
 	def move(self):
 		rospy.Subscriber('/scan', LaserScan, self.callback_laser)
@@ -110,6 +92,8 @@ class TurtleBot:
 			 	vel_msg = self.move_diag_right()
 			elif self.state == -1:
 				vel_msg = self.move_backwards()
+			elif self.state == -2:
+				vel_msg = self.stop()
 			else:
 				rospy.logerr('Unknown state!')
 
@@ -117,44 +101,44 @@ class TurtleBot:
 			self.rate.sleep()
 		
 	def shutdown(self):
-		self.velocity_publisher.publish(Twist()) # Stop movement
+		self.velocity_publisher.publish(self.stop())
 		rospy.loginfo("Shutting down")
 		rospy.sleep(1)
 		
 	def callback_laser(self, msg):
 	
 		laser_range = np.array(msg.ranges)
+		laser_range = [value if value <= self.max_dist else inf for value in laser_range]
 
 		left_dist = min(laser_range[60:120])
-		if (left_dist >= self.max_dist):
-			left_dist = inf
 		
 		front_dist = min(laser_range[0:60])
-		if (front_dist >= self.max_dist):
-			front_dist = inf
 
 		right_dist = min(laser_range[240:300])
-		if (right_dist >= self.max_dist):
-			right_dist = inf
 
-		print(left_dist, front_dist, right_dist)
-
-		if (front_dist >= inf and left_dist >= inf and right_dist >= inf):
-			self.state = 0
-		elif (min(left_dist, front_dist, right_dist) < self.ideal_dist and front_dist < self.ideal_dist):
+		if all(value >= inf for value in laser_range):
+			self.state = -2 # STOP
+			rospy.loginfo("Stopped")
+		elif (front_dist < self.target_dist):
 			self.state = -1
-		elif (left_dist <= front_dist and left_dist < right_dist):
+		elif ((left_dist <= front_dist and left_dist < right_dist)
+			or (front_dist < right_dist or front_dist < self.target_dist)
+			or (front_dist < self.target_dist and right_dist < self.target_dist)
+			or (right_dist < self.min_dist)):
 			self.state = 1
-		elif (front_dist < right_dist or front_dist < self.ideal_dist):
-			self.state = 1
-		else:
-			if (right_dist < self.ideal_dist):
-				self.state = 3
+		elif (right_dist < inf):
+			if (right_dist < self.target_dist):
+				self.state = 0
 			else:
 				self.state = 4
+		else:
+			# There is something on the back
+			self.state = 1
 
-		print(self.state)
-		
+
+		if (self.state != -2):
+			print(left_dist, front_dist, right_dist)
+			print(self.state)
 		
 
 		# if (front_dist < right_dist):
